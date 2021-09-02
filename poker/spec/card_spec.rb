0284126c130e2e2a8ec4817b493c590a5a509582
pierre <pnavarin@gmail.com>
2021-09-02 15:00:48 -0400

    Finished Card, started Deck

--- poker/spec/card_spec.rb
+++ poker/spec/card_spec.rb
@@ -1,1 +1,14 @@
 require "card"
+
+
+describe 'Card' do
+    subject(:c) {Card.new(4, :hearts)}
+    describe '#initialize' do
+        it 'should contain a value and a suit' do
+            expect(c.value).to eq(4)
+            expect(c.suit).to eq(:hearts)
+        end
+    end
+
+
+end
--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -1,1 +1,15 @@
 require "deck"
+
+describe 'Deck' do
+    subject(:d) {Deck.new}
+    describe '#initialize' do
+        it 'should contain 52 unique card instances' do 
+            expect(d.deck_arr.length).to eq(52)
+            expect(d.deck_arr.uniq.length).to eq(52)
+            expect(d.deck_arr.all? {|card| card.is_a?(Card)}).to be(true)
+        end
+    end 
+
+
+
+end
