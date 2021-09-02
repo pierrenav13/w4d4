0c6e83130f45c709de4d70da02fda96eadb7b9f2
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-09-02 16:55:20 -0400

    added pair?

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -1,11 +1,30 @@
+require_relative "deck"
+require_relative "card"
 class Hand
-    attr_accessor :dealt_hand, :shuffled_deck
+    attr_accessor :dealt_hand, :shuffled_deck, :deck
+    
     def initialize(deck)
         @deck = deck
         @shuffled_deck = deck.shuffle
         @dealt_hand = []
     end
 
+    def deal_hand
+        until @dealt_hand.length == 5
+            new_card = @shuffled_deck.pop 
+            @dealt_hand << new_card
+        end
+    end
 
-
+    def pair?
+        count = 0
+        @dealt_hand.each_with_index do |card1, i|
+            @dealt_hand.each_with_index do |card2, j|
+                if i < j
+                    count += 1 if card1.value == card2.value 
+                end
+            end
+        end
+        count == 1
+    end
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -8,13 +8,6 @@ describe 'Hand' do
         it 'should take in a deck instance' do
             expect(h.deck).to eq(deck)
         end
-        # describe '#dealt_hand' do
-        #     it 'should contain 5 shuffled unique cards' do
-        #         expect(h.dealt_hand.length).to eq(5)
-        #         expect(h.dealt_hand.uniq.length).to eq(5)
-        #         expect(h.dealt_hand.all?{|card| card.is_a?(Card)}).to be(true)
-        #     end 
-        # end
     end
 
     describe '#deal_hand' do
@@ -22,6 +15,9 @@ describe 'Hand' do
             expect(h.dealt_hand).to eq([])
             h.deal_hand
             expect(h.dealt_hand.length).to eq(5)
+        end
+        it "each card should be unique" do
+            h.deal_hand
             expect(h.dealt_hand.uniq.length).to eq(5)
             expect(h.dealt_hand.all?{|card| card.is_a?(Card)}).to be(true)
         end
@@ -29,6 +25,29 @@ describe 'Hand' do
             expect(h.shuffled_deck.length).to eq(52)
             h.deal_hand
             expect(h.shuffled_deck.length).to eq(47)
+        end
+    end
+
+    describe "#pair?" do 
+        it "should return true if there's only one set of matching card values " do 
+            h.dealt_hand = [
+            Card.new(2, :hearts),
+            Card.new(2, :hearts),
+            Card.new(4, :hearts),
+            Card.new(5, :hearts),
+            Card.new(7, :hearts)
+            ]
+            expect(h.pair?).to be(true)
+        end
+        it "should return false if there are no matching card values" do 
+            h.dealt_hand = [
+            Card.new(9, :hearts),
+            Card.new(2, :hearts),
+            Card.new(4, :hearts),
+            Card.new(5, :hearts),
+            Card.new(7, :hearts)
+            ]
+            expect(h.pair?).to be(false)
         end
     end
 
